52508998989de4ebeb4278b5db0b644c0a6159f4
Dominic <dominic.webandcrafts@gmail.com>
2020-07-03 09:33:13 +0530

    fix text

--- src/components/Overview/overview.js
+++ src/components/Overview/overview.js
@@ -17,7 +17,7 @@ const Overview = props => {
                 <div className="container">
                     <h1>Overview</h1>
                     <p>
-                        Baaz Framework is an open source UI toolkit for building performant, high-quality mobile and desktop apps using web technologies — HTML, CSS, and JavaScript — with integrations for popular frameworks like React.
+                        Baaz Framework is an open source UI toolkit for building performant, high-quality Web apps  — HTML, CSS, and JavaScript — with integrations for popular frameworks like React.
                     </p>
                     <p>
                         Get started building by <Link to={resourceUrl('/cli')}>installing Baaz</Link> or following our First App Tutorial to learn the main concepts.
